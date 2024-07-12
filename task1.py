68bb8d03241270ec63223d92201a943d21434e4b
PARAS MOHAN <paras09114@gmail.com>
2024-07-13 03:24:19 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,16 @@
+#programm TO CREATE STRING REVERSAL IN THE PYTHON..
+
+message=input("ENTER YOUR MESSAGE PLEASE\n")
+print("the original string is :",message)
+
+def reverse():
+    reverse_string=message[::-1]
+    print("the reverse string is the:",reverse_string)
+    
+reverse()    
+    
+
+
+
+
+
